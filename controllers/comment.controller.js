5ac767b2cf1b2b7ded01478f3d9638928041b17a
shaikhfurquan <shaikhfurquan313@gmail.com>
2025-01-22 21:05:22 +0530

    update and delete comment api

--- controllers/comment.controller.js
+++ controllers/comment.controller.js
@@ -1,5 +1,3 @@
-import UserModel from "../models/user.model.js";
-import VideoModel from "../models/video.model.js";
 import CommentModel from "../models/comment.model.js";
 
 
@@ -27,13 +25,63 @@ export const newComment = async (req, res, next) => {
 // get all comments for any video
 export const getAllCommentsOnVideo = async (req, res, next) => {
     try {
-        const comments = await CommentModel.find({ videoId: req.params.videoId }).populate("commentedByUserId" ,  "userName channelName logo.url")
+        const comments = await CommentModel.find({ videoId: req.params.videoId }).populate("commentedByUserId", "userName channelName logo.url")
         if (!comments) {
             return res.status(404).json({ message: "No comments found for this video" })
         }
         res.status(200).json({
             message: "Comments fetched successfully",
             commentsLists: comments
+        })
+    } catch (error) {
+        if (error.name === "CastError") {
+            return res.status(400).json({ message: "Invalid ID", error: error.message });
+        }
+        next(error);
+    }
+}
+
+
+export const updateComment = async (req, res, next) => {
+    try {
+        const comment = await CommentModel.findById(req.params.commentId);
+        if (!comment) {
+            return res.status(404).json({ message: "Comment not found" })
+        }
+        // check the ownership of the comment
+        if (comment.commentedByUserId.toString() != req.user._id.toString()) {
+            return res.status(403).json({ message: "You are not allowed to update this comment" })
+        }
+        // update the comment
+        comment.commentText = req.body.commentText
+        await comment.save()
+        res.status(200).json({
+            message: "Comment updated successfully",
+            updatedComment: comment
+        })
+    } catch (error) {
+        if (error.name === "CastError") {
+            return res.status(400).json({ message: "Invalid ID", error: error.message });
+        }
+        next(error);
+    }
+}
+
+
+export const deleteComment = async (req, res, next) => {
+    try {
+        const comment = await CommentModel.findById(req.params.commentId);
+        if (!comment) {
+            return res.status(404).json({ message: "Comment not found" })
+        }
+        // check the ownership of the comment
+        if (comment.commentedByUserId.toString() != req.user._id.toString()) {
+            return res.status(403).json({ message: "You are not allowed to delete this comment" })
+        }
+        // delete the comment
+        await CommentModel.findByIdAndDelete(req.params.commentId)
+        res.status(200).json({
+            message: "Comment deleted successfully"
         })
     } catch (error) {
         if (error.name === "CastError") {
--- routes/comment.route.js
+++ routes/comment.route.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import { isAuth } from '../middlewares/isAuth.middleware.js';
-import { getAllCommentsOnVideo, newComment } from '../controllers/comment.controller.js';
+import { deleteComment, getAllCommentsOnVideo, newComment, updateComment } from '../controllers/comment.controller.js';
 
 const commentRouter = express.Router();
 
-commentRouter.post('/new-comment/:videoId', isAuth , newComment)
-commentRouter.get('/comments/:videoId', isAuth , getAllCommentsOnVideo)
+commentRouter.post('/new-comment/:videoId', isAuth, newComment)
+commentRouter.get('/comments/:videoId', isAuth, getAllCommentsOnVideo)
+commentRouter.put('/update/:commentId', isAuth, updateComment)
+commentRouter.delete('/delete/:commentId', isAuth, deleteComment)
 
 
 export default commentRouter
